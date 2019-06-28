9e869902913f23fe3b8743c53761a17a8caf1aff
Chris Sun <sunhang0823@gmail.com>
2019-06-28 16:21:56 -0400

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/com/hangsun/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/hangsun/petclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.hangsun.petclinic.services;
 
 import com.hangsun.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
