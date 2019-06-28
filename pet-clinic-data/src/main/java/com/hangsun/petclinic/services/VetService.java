c9048a6b49f216ec1f9f76975b2fd953ee851d7f
Chris Sun <sunhang0823@gmail.com>
2019-06-28 14:45:15 -0400

    Implement Interfaces for servicews. close #5

---
+++ pet-clinic-data/src/main/java/com/hangsun/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.hangsun.petclinic.services;
+
+import com.hangsun.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
