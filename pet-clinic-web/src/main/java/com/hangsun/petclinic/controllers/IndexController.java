5cd0b84051e20af3a9a6bf18187001395ed73f67
Chris Sun <sunhang0823@gmail.com>
2019-06-28 17:08:50 -0400

    adding index page controller. close #8

---
+++ pet-clinic-web/src/main/java/com/hangsun/petclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.hangsun.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
