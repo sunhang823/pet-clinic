871b44acb0e402696881da6899f404ad13394f35
Chris Sun <sunhang0823@gmail.com>
2019-07-08 17:04:19 -0400

    added spring data jpa service for owners. closes #40

---
+++ pet-clinic-data/src/main/java/com/hangsun/petclinic/services/springdatajpa/OwnerSDJpa.java
@@ -0,0 +1,60 @@
+package com.hangsun.petclinic.services.springdatajpa;
+
+import com.hangsun.petclinic.model.Owner;
+import com.hangsun.petclinic.repositories.OwnerRepository;
+import com.hangsun.petclinic.repositories.PetRepository;
+import com.hangsun.petclinic.repositories.PetTypeRepository;
+import com.hangsun.petclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpa implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerSDJpa(OwnerRepository ownerRepository, PetRepository petRepository,
+                      PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        ownerRepository.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+}
