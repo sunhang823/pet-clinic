527726854a1e8bf44edef28c7128420c244566a9
Chris Sun <sunhang0823@gmail.com>
2019-07-08 17:25:26 -0400

    added specilty sd jpa service. closes #44

--- pet-clinic-data/src/main/java/com/hangsun/petclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/hangsun/petclinic/services/map/VetMapService.java
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
     private final SpecialtyService specialtyService;
 
-    public VetServiceMap(SpecialtyService specialityService) {
+    public VetMapService(SpecialtyService specialityService) {
         this.specialtyService = specialityService;
     }
 
